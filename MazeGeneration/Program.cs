d1b6bd6b5177828829d9b165d4d42f14422a2378
Dingleberryipod <a02330716@usu.edu>
2024-01-29 01:19:42 -0700

    idek

---
+++ MazeGeneration/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+namespace MazeGeneration {
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Maze yeah = new Maze(10,10);
+            yeah.displayMap();
+
+            yeah.displayEdges();
+
+            
+    
+
+    }
+
+
+
+    }
+}
